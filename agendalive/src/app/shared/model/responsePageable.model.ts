019c884225035ffb78a89f64641d569fb382199d
Fábio Sales <fabio.oliveira.sales@gmail.com>
2020-06-14 17:54:00 -0300

    criando service

---
+++ agendalive/src/app/shared/model/responsePageable.model.ts
@@ -0,0 +1,12 @@
+export class ResponsePageable{
+  content: any[];
+  firts: boolean;
+  last: boolean;
+  number: number;
+  numberOfElements: number;
+  pageable: any[];
+  size: number;
+  sort: number;
+  totalElements: number;
+  tatalPages: number;
+}
